cc96b90c0d4d3ad78836741f179990fde2b5c8ae
AlexisK <alexisk@jollix.me>
2016-04-27 18:40:44 +0300

    music parsing

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 angular.module('app', ['directives'])
 
-.service('vkAuth', function() {
+.controller('Audiolist', ['$scope', 'vkGetMusic', function($scope, vkGetMusic) {
+    var self = this;
+    
+    self.current = null;
+    
+    vkGetMusic.then(function(vkSession) {
+        console.log('Audiolist:Got session and music!');
+    });
+    
+}])
+
+.factory('vkAuth', function() {
+    var vkSession = null;
+    
     var promise = new Promise(function(done, reject) {
+        
         VK.Auth.getLoginStatus(function(response) {
             if (response.session) {
-                window.vkSession = response.session
+                vkSession = response.session
                 console.log('user: '+vkSession.mid);
                 done(vkSession);
             } else {
@@ -17,16 +31,33 @@ angular.module('app', ['directives'])
     return promise;
 })
 
-.controller('Audiolist', function($scope) {
-    var self = this;
-    self.bands = {
-        1: 'Cheville'
-    }
-    self.songs = [{band:1,title:'The Red', file: 'file'}];
-    self.current = null;
-    
-    vkAuth.then(function(vkSession) {
-        console.log('Got session!');
+.factory('vkGetMusic', ['vkAuth', 'parseAudioDb', function(vkAuth, parseAudioDb) {
+    var promise = new Promise(function(done, reject) {
+        
+        vkAuth.then(function(vkSession) {
+            console.log('vkGetMusic: Got session!');
+            
+            VK.Api.call('audio.get',{},function(list){
+                console.log('vkGetMusic: Got music!');
+                done(vkSession, parseAudioDb(list));
+            });
+            
+            
+        });
+        
     });
-    
+    return promise;
+}])
+
+.factory('parseAudioDb', function() {
+    return function(list) {
+        var db = {
+            artist: {},
+            song: {}
+        };
+        
+        
+        
+        return list
+    }
 });
