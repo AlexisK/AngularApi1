b2daac38e87d862fb5af4882698de3477d3f26e3
AlexisK <alexisk@jollix.me>
2016-04-28 14:55:41 +0300

    musiclist

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 angular.module('app', ['directives'])
 
+.directive('toggleClass', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function() {
+                element.toggleClass(attrs.toggleClass);
+            });
+        }
+    };
+})
+
+
 .controller('Audiolist', ['$scope', 'vkGetMusic', function($scope, vkGetMusic) {
     var self = this;
     
     self.current = null;
+    self.songs = [];
+    self.artists = [];
     
-    vkGetMusic.then(function(vkSession, musicDb) {
-        console.log('Audiolist:Got session and music!');
+    vkGetMusic(function(vkSession, music) {
+        console.log('Audiolist:Got session and music!\n', music);
+        self.songs = music.song;
+        self.artists = music.artist;
+        $scope.$apply();
     });
     
 }])
+
+
+
 
 .factory('vkAuth', function() {
     var vkSession = null;
@@ -32,32 +52,80 @@ angular.module('app', ['directives'])
 })
 
 .factory('vkGetMusic', ['vkAuth', 'parseAudioDb', function(vkAuth, parseAudioDb) {
-    var promise = new Promise(function(done, reject) {
-        
+    
+    return function(todo) {
         vkAuth.then(function(vkSession) {
             console.log('vkGetMusic: Got session!');
             
-            VK.Api.call('audio.get',{},function(list){
+            VK.Api.call('audio.get',{},function(resp){
                 console.log('vkGetMusic: Got music!');
-                done(vkSession, parseAudioDb(list));
+                todo(vkSession, parseAudioDb(resp.response));
             });
             
-            
         });
-        
-    });
-    return promise;
+    };
 }])
 
 .factory('parseAudioDb', function() {
+    
+    var trimName = function(name) {
+        var newName = name.toLowerCase().trim().replace(/\s+/g,' ').split(/(feat|\(|\[)/g)[0].split(' ');
+        
+        if ( newName.length > 3 ) {
+            if ( newName[0] == 'the' ) { newName.splice(0,1); }
+        }
+        angular.forEach(newName, function(v, i) {
+            var nval = v;
+            if ( v.length > 3 && v[v.length-1] == 's') {
+                nval = v.slice(0,-1);
+            }
+            newName[i] = nval.replace(/\W/g,'');
+        });
+        
+        return newName.join('');
+    }
+    
+    var artistToIndex = {};
+    var artistIndex = 0;
+    
+    
+    
     return function(list) {
         var db = {
-            artist: {},
-            song: {}
+            artist: [],
+            song: []
         };
         
+        angular.forEach(list, function(val) {
+            var artistName = trimName(val.artist);
+            var ind = artistToIndex[artistName];
+            if ( !ind ) {
+                
+                var newArtist = {
+                    title: val.artist,
+                    name: artistName,
+                    songs: []
+                };
+                
+                ind = db.artist.push(newArtist)-1;
+                artistToIndex[artistName] = ind;
+            }
+            var artist = db.artist[ind];
+            
+            var newSong = {
+                title: val.title,
+                dur: val.duration,
+                artist: ind,
+                url: val.url
+            }
+            
+            db.song.push(newSong);
+            
+            artist.songs.push(newSong);
+        });
         
+        console.log(list[0]);
         
-        return list
+        return db;
     }
 });
